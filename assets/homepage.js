ccd26bad8eababbdda0a21e24f4ad329ce301d97
rhare86 <rhare86@gmail.com>
2022-05-01 16:10:28 -0500

    rudimentary search form added

--- assets/homepage.js
+++ assets/homepage.js
@@ -1,27 +1,36 @@
 
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Host': 'tasty.p.rapidapi.com',
+		'X-RapidAPI-Key': '0dc28fbcb9mshb78abb346d445efp1333a3jsn79268baadc63'
+	}
+};
+let apiUrl = "https://tasty.p.rapidapi.com/recipes/list?from=0&size=30";
+var getRecipes = function() {
+    fetch (apiUrl, options)
+    .then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+};
+
+
+
 var mealButtonsEl = document.querySelector("div", "#meal-buttons");
 
+/*     fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=30&tags=vegetarian', options)
+*/
+
+getRecipes();
 
 
 
-/*
-var getRecipes = function(meal){
-    var apiUrl = "https://tasty.p.rapidapi.com/recipes/auto-complete";
-        fetch(apiUrl).then(function(response){
-            if (response.ok) {
-                response.json().then(function(data){
-                    displayRecipes(data.items, meal);
-                });
-            } else {
-                alert("Error: Recipe Not Found.");
-            }
-        });
-};
 
-var getRecipes = function() {
-    fetch('https://api.spoonacular.com/recipes/complexSearch');
-};
-getRecipes(); */
+
+
+/*  
 
 const options = {
 	method: 'GET',
@@ -31,9 +40,9 @@ const options = {
 	}
 };
 
-fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=30&tags=breakfast', options)
+fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=30', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
 
-
+    */
