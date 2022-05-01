156464b0171d65757b947201dac1809bac8124c2
rhare86 <rhare86@gmail.com>
2022-05-01 14:25:08 -0500

    tast api linked in js file

--- assets/homepage.js
+++ assets/homepage.js
@@ -1,9 +1,10 @@
+
 var mealButtonsEl = document.querySelector("div", "#meal-buttons");
 
 
 
 
-
+/*
 var getRecipes = function(meal){
     var apiUrl = "https://tasty.p.rapidapi.com/recipes/auto-complete";
         fetch(apiUrl).then(function(response){
@@ -15,5 +16,24 @@ var getRecipes = function(meal){
                 alert("Error: Recipe Not Found.");
             }
         });
-
 };
+
+var getRecipes = function() {
+    fetch('https://api.spoonacular.com/recipes/complexSearch');
+};
+getRecipes(); */
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Host': 'tasty.p.rapidapi.com',
+		'X-RapidAPI-Key': '0dc28fbcb9mshb78abb346d445efp1333a3jsn79268baadc63'
+	}
+};
+
+fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=30&tags=breakfast', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+
+
