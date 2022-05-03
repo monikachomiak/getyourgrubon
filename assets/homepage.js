f67b23f43f458fceeea68448ecc27c6aed74c9b9
rhare86 <rhare86@gmail.com>
2022-05-02 20:07:57 -0500

    fixed api url

--- assets/homepage.js
+++ assets/homepage.js
@@ -1,4 +1,3 @@
-
 const options = {
 	method: 'GET',
 	headers: {
@@ -6,43 +5,61 @@ const options = {
 		'X-RapidAPI-Key': '0dc28fbcb9mshb78abb346d445efp1333a3jsn79268baadc63'
 	}
 };
-let apiUrl = "https://tasty.p.rapidapi.com/recipes/list?from=0&size=30";
-var getRecipes = function() {
-    fetch (apiUrl, options)
+var recipeFormEl = document.querySelector("#recipe-form");
+var ingredientInputEl = document.querySelector("#ingredient");
+var recipesContainerEl = document.querySelector("#recipes-container");
+var ingredientSearchTerm = document.querySelector("#ingredient-search-term");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    var ingredient = ingredientInputEl.value.trim();
+        if (ingredient) {
+            console.log(ingredient);
+            getRecipes(ingredient);
+            ingredientInputEl.value = "";
+        } else {
+            alert("Please enter a main ingredient");
+        }
+
+    console.log(event);
+};
+
+var getRecipes = function(ingredient) {
+    let apiUrl = "https://tasty.p.rapidapi.com/recipes/list?from=0&size=30&q=" + ingredient;
+    fetch(apiUrl, options)
     .then(function(response) {
-        response.json().then(function(data) {
-            console.log(data);
-        });
+        response.json()
+        .then(function(data) {
+            displayRecipes(data, ingredient);
+        }).catch(function(error) {
+            console.log(error);
+        })
+
     });
 };
 
+var displayRecipes = function(recipes, searchTerm) {
+    console.log(recipes);
+    console.log(searchTerm);
+    recipesContainerEl.textContent = "";
+    ingredientSearchTerm.textContent = searchTerm;
 
+    for (var i=0; i < recipes.length; i++) {
+        var recipeName = recipes[i].description;
+        var recipeEl = document.createElement("div");
+        recipeEl.classList = "list-item flex-row justify-space-between align-center";
+        var titleEl = document.createElement("span");
+        titleEl.textContent = recipeName;
 
-var mealButtonsEl = document.querySelector("div", "#meal-buttons");
+        recipeEl.appendChild(titleEl);
 
-/*     fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=30&tags=vegetarian', options)
-*/
+        recipesContainerEl.appendChild(recipeEl);
+    }
 
-getRecipes();
-
-
-
-
-
-
-/*  
-
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Host': 'tasty.p.rapidapi.com',
-		'X-RapidAPI-Key': '0dc28fbcb9mshb78abb346d445efp1333a3jsn79268baadc63'
-	}
 };
 
-fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=30', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+recipeFormEl.addEventListener("submit", formSubmitHandler);
+/*     fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=30&tags=vegetarian', options)  
 
     */
+
