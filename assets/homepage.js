5af3bbcc484edbdbbfe3174cdb63d349b2c4a0a9
rhare86 <rhare86@gmail.com>
2022-05-08 23:03:28 -0500

    reicpe search only to the console

--- assets/homepage.js
+++ assets/homepage.js
@@ -1,100 +1,141 @@
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Host': 'tasty.p.rapidapi.com',
+		'X-RapidAPI-Host': 'yummly2.p.rapidapi.com',
 		'X-RapidAPI-Key': '0dc28fbcb9mshb78abb346d445efp1333a3jsn79268baadc63'
 	}
 };
+// fetch('https://yummly2.p.rapidapi.com/feeds/search?start=0&maxResult=5&q=chicken%20soup'
+
+
+var recipeFormEl = document.querySelector("#recipe-search-form");
+var ingredientInputEl = document.querySelector("#ingredient");
 var recipesContainerEl = document.querySelector("#recipes-container");
+var ingredientSearchTermEl = document.querySelector("#ingredient-search-term");
+
+
+var getRecipes = function(ingredient) {
+	var apiUrl = "https://yummly2.p.rapidapi.com/feeds/search?start=0&maxResult=5&q=" + ingredient;
+	var response = fetch(apiUrl, options)
+	.then(function(response) {
+		response.json().then(function(data) {
+			displayRecipes(data, ingredient);
+	});
+});
+};
+
+var formSubmitHandler = function(event) {
+	event.preventDefault();
+	var recipe = ingredientInputEl.value.trim();
+
+		if (recipe) {
+			getRecipes(recipe);
+			ingredientInputEl.value = "";
+		} else {
+			alert("Please enter an ingredient.");
+		}
+	console.log(event);
+};
+
+var displayRecipes = function(recipes, ingredientSearchTermEl) {
+	console.log(recipes);
+	console.log(ingredientSearchTermEl);
+	recipesContainerEl.textContent = "";
+	ingredientSearchTermEl.textContent = ingredientSearchTermEl;
+
+		// loops recipes
+		for(var i=0; i < recipes.length; i++) {
+		
+			// format recipe name
+			var recipeName = recipes[i].feed.display + "/" + recipes[i].name;
+			
+			// container for each recipe
+			var recipeEl = document.createElement("div");
+			recipeEl.classList = "list-item";
+	
+			// span element to hold recipe name
+			var titleEl = document.createElement("span");
+			titleEl.textContent = recipeName;
+	
+			// append to container
+			recipeEl.appendChild(titleEl);
+	
+			// append container to the dom
+			recipesContainerEl.appendChild(recipeEl);
+	
+			} 
+};
+
+recipeFormEl.addEventListener("submit", formSubmitHandler);
+
+
+
+
+/*var recipesContainerEl = document.querySelector("#recipes-container");
 var ingredientSearchTerm = document.querySelector("#ingredient-search-term");
 var recipeFormEl = document.querySelector("#recipe-form");
-var ingredientInputEl = document.querySelector("#ingredient");
+var ingredientInputEl = document.querySelector("#recipe");
+
+
+/* var getRecipes = function(recipe) {
+	let apiUrl = "https://tasty.p.rapidapi.com/recipes/list?from=0&size=5" + recipe;
+		var response = fetch(apiUrl, options).then(function(response) {
+			response.json().then(function(data) {
+				displayRecipes(data, recipe);
+			});
+		});
+	};				
 
 
 
-var getRecipes = function(ingredient) {
-    let apiUrl = "https://tasty.p.rapidapi.com/recipes/list?from=0&size=5&q=" + ingredient;
-    var response = fetch(apiUrl, options);
-    fetch(apiUrl, options).then(function(response) {
-        response.json().then(function(data) {
-            console.log(data);
-        });
-    });
-};
-
-var formSubmitHandler = function(event) {
-    event.preventDefault();
-    console.log(event);
-};
-
-recipeFormEl.addEventListener("submit", formSubmitHandler);
-    var ingredient = ingredientInputEl.value.trim();
-        if (ingredient) {
-            getRecipes(ingredient);
-            ingredientInputEl.value = "";
-        } else {
-            alert("Please enter a main ingredient.");
-        }
-
-
-/*
-var recipeFormEl = document.querySelector("recipe-form");
-var ingredientInputEl = document.querySelector("#ingredient");          
-var recipesContainerEl = document.querySelector("#recipes-container");
-var ingredientSearchTerm = document.querySelector("#ingredient-search-term");
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
-
-
-    var ingredient = ingredientInputEl.value.trim();
-        if (ingredient) {
-            console.log(ingredient);
-            getRecipes(ingredient);
-            ingredientInputEl.value = "";
-        } else {
-            alert("Please enter a main ingredient");
-        }
-
+	var recipe = ingredientInputEl.value.trim();
+		if (recipe) {
+			getRecipes(recipe);
+			ingredientInputEl.value = "";
+		} else {
+			alert("Please enter an ingredient");
+		}
     console.log(event);
 };
-var displayRecipes = function(recipes, searchTerm) {
-    console.log(recipes);
-    console.log(searchTerm);
-    recipesContainerEl.textContent = "";
-    ingredientSearchTerm.textContent = searchTerm;
 
-    for (var i=0; i < recipes.length; i++) {
-        var recipeName = recipes[i].description;
-        var recipeEl = document.createElement("div");
-        recipeEl.classList = "list-item flex-row justify-space-between align-center";
-        var titleEl = document.createElement("span");
-        titleEl.textContent = recipeName;
+var displayRecipes = function(recipe, ingredientSearchTerm) {
+	console.log(recipe);
+	console.log(ingredientSearchTerm);
+	
+	document
+	.querySelector("#recipe-container")
+	recipesContainerEl.textContent = "";
+	ingredientSearchTerm.textContent = ingredientSearchTerm;
+		
+		// loops recipes
+		for(var i=0; i < recipe.length; i++) {
+		
+		// format recipe name
+		var recipeName = recipe[i].show.id + "/" + recipe[i].name;
+		
+		// container for each recipe
+		var recipeEl = document.createElement("div");
+		recipeEl.classList = "list-item";
 
-        recipeEl.appendChild(titleEl);
+		// span element to hold recipe name
+		var titleEl = document.createElement("span");
+		titleEl.textContent = recipeName;
 
-        recipesContainerEl.appendChild(recipeEl);
-    }
+		// append to container
+		recipeEl.appendChild(titleEl);
 
-};
+		// append container to the dom
+		recipesContainerEl.appendChild(recipeEl);
 
-var getRecipes = function(ingredient) {
-    let apiUrl = "https://tasty.p.rapidapi.com/recipes/list?from=0&size=30&q=" + ingredient;
-    fetch(apiUrl, options)
-    .then(function(response) {
-        response.json()
-        .then(function(data) {
-            displayRecipes(data, ingredient);
-        }).catch(function(error) {
-            console.log(error);
-        })
-
-    });
-};
-
-
+		}
+}
 
 recipeFormEl.addEventListener("submit", formSubmitHandler);
+
+
 /*     fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=30&tags=vegetarian', options)  
 
     */
